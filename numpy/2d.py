ef494b054716276d1e05262d606447c7ce23faec
AmjadAlit <amjadalit171@gmail.com>
2026-01-16 12:03:19 +0530

    Add files via upload

---
+++ numpy/2d.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+# a=np.array([[1,2,5,4],[4,5,2,7],[8,6,10,7,]],dtype=float)
+# b=np.sort(a)
+# print(b)
+# print(a.dtype)
+
+# b=np.zeros([2,3],dtype=int)
+# print(b)
+# print(b.dtype)
+
+# c=np.zeros([2,3,3])
+# print(c)
+# print(c.ndim)
+
+# a=np.ones([2,3])
+# print(a)
+
+# a=np.full([1,3,4],4)
+# print(a)
+# print(a.ndim)
+
+# a=np.identity(3)
+# print(a)
+
+import pandas as pd
+print(pd.__version__)
