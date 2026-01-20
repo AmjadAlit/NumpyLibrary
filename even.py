dbb9583c474e39b38c59d6b04f9f4c673eceb22c
AmjadAlit <amjadalit171@gmail.com>
2026-01-20 10:33:37 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+# print(np.__version__)
+
+# a=np.array([1,2,3,4,5,6])
+# print("array",a)
+# alphabet=np.array(['a,b,c,d,e'])
+# print("array",alphabet)
+
+# vowel=np.array(['a','e','o','i','u'])
+# print("array",vowel)
+
+# a=np.array([1,2,3,4,5,6])
+# print("sum:",np.sum(a))
+# print("mean:",np.mean(a))
+# print(type(a))
+
+# a=np.array(['a','b','c''d'])
+# print(type(a))
+# print(a.ndim)
+
+# b=np.array([[1,2,3],[4,5,6]])
+# print(b)
+# # print(b.ndim)
+# print(b.shape)
+
+# a2=np.array([[1,2,3,4],[5,6,7,8]])
+# print(a2)
+# print(a2.ndim)
+# print(a2.shape)
+
+a3=np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(a3)
+print(a3.ndim)
+print(a3.shape)
+
+
+
+
---
+++ example.py
@@ -0,0 +1,53 @@
+import numpy as np 
+
+# a=np.array([1,2,3,4,5,6])
+# print("array",a)
+
+# alphabet=np.array(['a','b','c','d'])
+# print("array",alphabet)
+
+# vowel=np.array(['a','e','i','o','u'])
+# print("array",vowel)
+
+# a=np.array([1,2,3,4,5,6,7])
+# print("sum:",np.sum(a))
+# print("mean:",np.mean(a))
+# print(type(a))
+# b=np.array(['a','b','c','d'])
+# print(type(b))
+# print(a.ndim)
+# b=np.array([[1,2,3],[4,5,6,]])
+# print("array",b)
+# print(b.ndim)
+# print(b.shape)
+
+# a1=np.array([[1,2,3,],[5,6,7,],[9,10,11,],[13,14,15,]])
+# print(a1)
+# print(a1.ndim)
+# print(a1.shape)
+
+# import array 
+
+# arr=array.array('i',[10,20,30,40,50])
+# print(arr)
+# print("first element:",arr[0])
+# print("third element:",arr[3])
+# arr.append(60)
+# print(arr)
+# arr.extend([12,14])
+# print(arr)
+# arr.insert(1,60)
+# print(arr)
+# arr.pop(2)
+# arr.remove(10)
+# print(arr)
+# print(arr[0:3])
+# print(arr[1:4])
+# print(arr[2:5])
+# print(arr[-5:-1])
+
+
+
+
+
+
